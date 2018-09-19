eb9b19038252aeb601e8c16a19565f5b6d932587
Ariel Zhen <pzhen001@g.ucla.edu>
2018-09-19 16:57:00 -0700

    get '/books' route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,13 @@
 class BooksController < ApplicationController
 
   get '/books' do
-    
+    if logged_in?
+      @user = current_user
+      @books = Book.all
+      erb :'/books/books'
+    else
+      redirect to '/login'
+    end
   end
 
   get '/books/new' do
