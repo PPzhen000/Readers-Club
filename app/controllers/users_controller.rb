4513ce31bdfa9d21c33b39773faff970b3ccea58
Ariel Zhen <pzhen001@g.ucla.edu>
2018-09-18 22:41:15 -0700

    created UsersController routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+require 'pry'
+
+  get '/signup' do
+
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/signin' do
+
+  end
+
+  post '/signin' do
+
+  end
+
+  get '/users/:slug' do
+
+  end
+
+  get '/logout' do
+
+  end
+
+end
