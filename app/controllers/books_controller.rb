3e59d120b7eda4ebc498f9d14dc18b2963466f41
Ariel Zhen <pzhen001@g.ucla.edu>
2018-09-19 10:41:04 -0700

    created BooksController

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,28 @@
+class BooksController < ApplicationController
+
+  get '/books' do
+    
+  end
+
+  get '/books/new' do
+
+  end
+
+  post '/books' do
+
+  end
+
+  get '/books/:slug/edit' do
+
+  end
+
+  patch '/books/:slug' do
+
+  end
+
+  delete '/books/:slug/delete' do
+
+  end
+
+
+end
