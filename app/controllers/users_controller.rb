8462321f2b16975e89d9c96b4737c58a11992c4b
Ariel Zhen <pzhen001@g.ucla.edu>
2018-09-19 10:33:37 -0700

    created 'signin' route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,15 +11,32 @@ require 'pry'
   end
 
   post '/signup' do
-
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to '/books'
+    end
   end
 
   get '/signin' do
-
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/books'
+    end
   end
 
   post '/signin' do
-
+    @user = User.find_by(:username => params[:username])
+      # binding.pry
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/books'
+    else
+      redirect to '/login'
+    end
   end
 
   get '/users/:slug' do
