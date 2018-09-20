7c5ae4fb4c41342df901476c203666d6087b6e96
Ariel Zhen <pzhen001@g.ucla.edu>
2018-09-20 15:41:33 -0700

    added Book helper methods

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,13 @@ class Book < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :categories
 
+  def slug
+    title.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Book.all.find{|book| book.slug == slug}
+  end
+
+
 end
