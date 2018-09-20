b7fae414ef64a8f6bf84ef0416912981b005dc2e
Ariel Zhen <pzhen001@g.ucla.edu>
2018-09-20 12:39:18 -0700

    migrated categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180919053339) do
+ActiveRecord::Schema.define(version: 20180920193714) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20180919053339) do
   create_table "books_users", id: false, force: :cascade do |t|
     t.integer "book_id"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
