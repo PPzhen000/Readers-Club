41ab82dbc712dd4518993b9eeb1e3afabc2db904
Ariel Zhen <pzhen001@g.ucla.edu>
2018-09-20 13:01:41 -0700

    added book datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,28 @@
-
+book_list = {
+  "Crazy Rich Asians" => {
+      :author_name => "Kevin Kwan"
+    },
+  "Origin" => {
+      :author_name => "Dan Brown"
+    },
+  "Fear" => {
+      :author_name => "Bob Woodward"
+    },
+  "To Kill a Mockingbird" => {
+      :author_name => "Harper Lee"
+    },
+  "Pride and Prejudice" => {
+      :author_name => "Jane Austen"
+    },
+  "The Alchemist" => {
+      :author_name => "Paulo Coelho"
+    },
+}
+book_list.each do |title, book_hash|
+  p = Book.new
+  p.title = title
+  book_hash.each do |attribute, value|
+      p[attribute] = value
+  end
+  p.save
+end
